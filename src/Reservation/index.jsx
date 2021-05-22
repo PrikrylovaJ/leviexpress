faae256444bb54246a8b1af5528b0bc8021e9ad5
PrikrylovaJ <jprikrylova@email.cz>
2021-05-22 11:32:32 +0200

    Routovaní

---
+++ src/Reservation/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Reservation = () => {
+  return  <h2>Ahoj</h2>
+};
+
+export default Reservation;
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './style.css';
 import Header from './Header';
 import Footer from './Footer';
+import Home from './Home';
+import Reservation from './Reservation';
 
 const App = () => (
   <>
-    <Header/>
-    <Footer/>
+    <Router>
+      <Header/>
+        <Switch>
+          <Route path="/reservation">
+            <Reservation />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      <Footer/>
+    </Router>
   </>
 );
+
+
 
 render(<App />, document.querySelector('#app'));
